b4499a5f7f3a706ffd3eeac2d0189207e31e2df7
DAKS54 <sylvain.diallo@epitech.eu>
2026-01-18 02:17:12 +0000

    [Update] add vercel endpoint in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const app = express();
 app.use(cors())
 app.use(express.json()) 
-app.use("/tasks",router)
+app.use("/tasks-phi-nine.vercel.app",router)
 
 app.get("/", (req, res)=>{
     res.send("Tout fonctionne bien ici")
