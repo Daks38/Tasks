8b3e59e571ab13ee55f7e899f3727d22c21b3346
DAKS54 <sylvain.diallo@epitech.eu>
2026-01-18 02:24:20 +0000

    [Update] app.listen to module.exports in  server.js

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,5 @@ app.use("/tasks",router)
 app.get("/", (req, res)=>{
     res.send("Tout fonctionne bien ici")
 })
-
-app.listen(process.env.PORT, ()=>console.log(`Vous êtes connecté sur http://localhost:${process.env.PORT}`))
+module.exports = app;
+// app.listen(process.env.PORT, ()=>console.log(`Vous êtes connecté sur http://localhost:${process.env.PORT}`))
