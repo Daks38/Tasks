90c0c004421f349b81bfdd264c0f28058e1ffe20
DAKS54 <sylvain.diallo@epitech.eu>
2026-01-19 16:41:55 +0000

    [Update] tasksRoutes,server and server

--- backend/Database/mongo.js
+++ backend/Database/mongo.js
@@ -1,9 +1,24 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv").config();
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv").config();
 
-const mongo =  mongoose.connect(process.env.URI)
-  .then(() => {
+// const mongo =  mongoose.connect(process.env.URI)
+//   .then(() => {
+//     console.log("Mongo est fonctionnel...");
+//   })
+//   .catch((err) => console.log(err));
+
+// module.exports = mongo
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.URI);
     console.log("Mongo est fonctionnel...");
-  })
-  .catch((err) => console.log(err));
-module.exports = mongo
+  } catch (error) {
+    console.error("Erreur MongoDB :", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
