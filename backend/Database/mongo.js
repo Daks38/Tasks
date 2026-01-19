44404c835ab98bb31f8fe4195b816f0eb181a733
DAKS54 <sylvain.diallo@epitech.eu>
2026-01-19 17:34:31 +0000

    [Update] endpoints Detail and mongo

--- backend/Database/mongo.js
+++ backend/Database/mongo.js
@@ -1,13 +1,4 @@
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv").config();
 
-// const mongo =  mongoose.connect(process.env.URI)
-//   .then(() => {
-//     console.log("Mongo est fonctionnel...");
-//   })
-//   .catch((err) => console.log(err));
-
-// module.exports = mongo
 const mongoose = require("mongoose");
 require("dotenv").config();
 
@@ -22,3 +13,31 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+// const mongoose = require("mongoose");
+
+// let isConnected = false;
+
+// const connectDB = async () => {
+//   if (isConnected) return;
+
+//   try {
+//     await mongoose.connect(process.env.URI);
+//     isConnected = true;
+//     console.log("MongoDB connecté");
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+// module.exports = connectDB;
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv").config();
+
+// const mongo =  mongoose.connect(process.env.URI)
+//   .then(() => {
+//     console.log("Mongo est fonctionnel...");
+//   })
+//   .catch((err) => console.log(err));
+
+// module.exports = mongo
--- frontend/src/pages/Detail.jsx
+++ frontend/src/pages/Detail.jsx
@@ -31,7 +31,9 @@ const Detail = () => {
   return (
     <div>
       <section className="bg-white p-2 md:p-6 rounded-2xl border border-gray-300 max-w-xl mx-auto mt-[15vh]">
-        <span className="p-2 rounded-2xl bg-green-700 text-white">{task.createdAt.slice(0, 10)}</span>
+        <span className="p-2 rounded-2xl bg-green-700 text-white">
+          {task.createdAt.slice(0, 10)}
+        </span>
         <details open className="border-b border-gray-300">
           <summary className="outline-none list-none py-6 text-lg font-bold cursor-pointer relative flex justify-between rounded-lg select-none hover:after:opacity-75 focus-visible:ring-4 focus-visible:ring-gray-100">
             {task.title}
@@ -47,12 +49,12 @@ const Detail = () => {
           >
             Modifier
           </Link>
-          <Link
-            className="ml-auto w-1/2 bg-red-800 text-white p-2 rounded font-semibold hover:bg-red-900"
+          <button
             onClick={handleDelete}
+            className="ml-auto w-1/2 bg-red-800 text-white p-2 rounded font-semibold hover:bg-red-900"
           >
             Supprimer
-          </Link>
+          </button>
         </div>
       </section>
     </div>
